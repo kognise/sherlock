936ab0b110d5c1f82ebfc6e01d8ce379cb0623e2
Felix Mattick <felix.mattick@gmail.com>
2018-08-28 18:11:27 -0400

    Fix main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sherlock
 path   = input('Path to search: ')
 string = input('Search for: ')
 
-searcher = new sherlock.Sherlock(path, 10)
-searcher.search(string)
+searcher = sherlock.Sherlock(path, 10)
+searcher.start(string)
