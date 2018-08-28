4ae31e1ac5d9def5253dfdd9b04e571ddac7333e
Felix Mattick <felix.mattick@gmail.com>
2018-08-28 17:00:22 -0400

    Add some comments

--- sherlock.py
+++ sherlock.py
@@ -2,6 +2,7 @@ import os
 import time
 
 class Sherlock():
+    # Initialization function
     def __init__(this, search_path='', max_threads=1024, search_file_names=True, search_file_contents=True):
         this.unique_id = this._get_unique_id()
 
@@ -11,6 +12,7 @@ class Sherlock():
         this.search_file_names    = search_file_names
         this.search_file_contents = search_file_contents
 
+    # Main functions
     def start(this):
         if (search_path == '' or max_threads <= 0 or (search_file_names == false and search_file_contents == false)):
             this.log('Nothing to do.')
@@ -19,7 +21,7 @@ class Sherlock():
             this.log('Search path doesn\'t exist.')
             return
 
-        // Do things.
+        # Do things.
 
     def log(this, message):
         string = '[%s] %s' % (this._get_time(), message)
@@ -28,6 +30,7 @@ class Sherlock():
         file.close()
         print(string)
 
+    # Helper functions
     def _get_unique_id(this):
         format_string = '%Y%m%d%H%M%S'
         output_string = time.strftime(format_string)
