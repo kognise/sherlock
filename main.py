45ec2f266a2eca3186ffac7dfc0bf8e6356e63eb
Felix Mattick <felix.mattick@gmail.com>
2018-08-28 18:22:17 -0400

    Change thread #

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sherlock
 path   = input('Path to search: ')
 string = input('Search for: ')
 
-searcher = sherlock.Sherlock(path, 10)
+searcher = sherlock.Sherlock(path, 2046)
 searcher.start(string)
